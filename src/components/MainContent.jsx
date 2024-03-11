613a9aac0e98b9f9efc9d80e72179747cdd01ba7
Tatyana Milanova <tatyana.milanova@mentormate.com>
2024-03-11 20:41:02 +0200

    Add env variables

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-import HelpArea from './HelpArea';
+import HelpArea from './HelpArea'
 
 function MainContent() {
-  const [helpVisible, setHelpVisible] = useState(false);
+  const [helpVisible, setHelpVisible] = useState(false)
 
   function toggleHelp() {
-    setHelpVisible((isVisible) => !isVisible);
+    setHelpVisible((isVisible) => !isVisible)
   }
 
   return (
     <main>
+      <h2>My DB name is ${process.env.DB_NAME}</h2>
       <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
       {helpVisible && <HelpArea />}
     </main>
-  );
+  )
 }
 
-export default MainContent;
+export default MainContent
